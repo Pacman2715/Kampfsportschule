6d350aff2e5d4dc1bf47dfee26bef65e178b667e
BenteWTF <97910454+BenteWTF@users.noreply.github.com>
2022-02-04 09:51:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,95 +17,131 @@ function connectToDatabase($server, $benutzer, $password, $database) {
 
 $dbLink = connectToDatabase("localhost", "root", "", "kampfsportschule");
 
-function abfrageID($dbLink){
+//Abfrage von einer Person
+function abfrageUser($dbLink){
     $url = $_GET['url'];
     $params = explode("/",$url);
     $query ="SELECT * FROM person WHERE id =('".$params[1]."')";
-    
     $result = mysqli_query($dbLink, $query);
-
     while($row=$result->fetch_assoc()){
         //echo "<option value='".$row['ID']."'>".$row['Vorname']."</option>";
-        $ID = $row['ID'];
+        $id = $row['ID'];
+        $vorname = $row['Vorname'];
+        $nachname = $row['Nachname'];
+        $geburtstag = $row['Geburtstag'];
+        $email = $row['E-Mail'];
     }
     
-    return $ID;
+    $query ="SELECT * FROM erfahrung WHERE Personen_ID =('".$params[1]."')";
+    $result = mysqli_query($dbLink, $query);
+    while($row=$result->fetch_assoc()){
+        //echo "<option value='".$row['ID']."'>".$row['Vorname']."</option>";
+        $erfErhaltungsdatum = $row['Erhaltungsdatum'];
+        $erfStil = $row['Stil_ID'];
+        $erfGuertel = $row['Guertel_ID'];
+    }
     
+    $query ="SELECT * FROM stil WHERE ID =('".$erfStil."')";
+    $result = mysqli_query($dbLink, $query);
+    while($row=$result->fetch_assoc()){
+        //echo "<option value='".$row['ID']."'>".$row['Vorname']."</option>";
+        $stilBeschreibung = $row['Stilbezeichnung'];
+    }
+    
+    $query ="SELECT * FROM guertel WHERE ID =('".$erfGuertel."')";
+    $result = mysqli_query($dbLink, $query);
+    while($row=$result->fetch_assoc()){
+        //echo "<option value='".$row['ID']."'>".$row['Vorname']."</option>";
+        $guertelFarbe = $row['Farbe'];
+    }
+    
+    return array($id, $vorname, $nachname, $geburtstag, $email, $erfErhaltungsdatum, $stilBeschreibung, $guertelFarbe);  
 }
 
-function abfrageVorname($dbLink){
+//Start und Stop eines Trainings
+function doTraining($dbLink){
     $url = $_GET['url'];
     $params = explode("/",$url);
-    $query ="SELECT * FROM person WHERE id =('".$params[1]."')";
+     $currentDateAndTime = date('Y-m-d h:i:s', time());  
+     if($params[2]=="setTrainingStart"){
+        $query = "INSERT INTO `kampfsportschule`.`training` (`Personen_ID`,`Zeit`,`Start/Stop`,`Stil_ID`) VALUES ('".$params[1]."','".$currentDateAndTime."','Start','".$params[3]."');";
+     
+        if ($dbLink->query($query) === TRUE) {
+            return true;
+        } else {
+            echo "Error: " . $query . "<br>" . $dbLink->error;
+        return false;
+     }
+     }else if ($params[2]=="setTrainingStop"){
+        $query = "INSERT INTO `kampfsportschule`.`training` (`Personen_ID`,`Zeit`,`Start/Stop`,`Stil_ID`) VALUES ('".$params[1]."','".$currentDateAndTime."','Stop','".$params[3]."');";
     
-    $result = mysqli_query($dbLink, $query);
-
-    while($row=$result->fetch_assoc()){
-        //echo "<option value='".$row['ID']."'>".$row['Vorname']."</option>";
-        $VORNAME = $row['Vorname'];
+        if ($dbLink->query($query) === TRUE) {
+           return true;
+        } else {
+           echo "Error: " . $query . "<br>" . $dbLink->error;
+           return false;
+        }
+    }else{
+        return false;
     }
+     
+     //$query = "INSERT INTO `kampfsportschule`.`training` (`Personen_ID`,`Zeit`,`Start/Stop`,`Stil_ID`) VALUES ('2','1665-02-03 12:26:01','Start','1');";
     
-    return $VORNAME;
-    
-}
+ } 
 
-function abfrageNachname($dbLink){
+// hinzufügen oder Löschen von Benutzern
+function doEditUser($dbLink){
     $url = $_GET['url'];
     $params = explode("/",$url);
-    $query ="SELECT * FROM person WHERE id =('".$params[1]."')";
-    
-    $result = mysqli_query($dbLink, $query);
-
-    while($row=$result->fetch_assoc()){
-        //echo "<option value='".$row['ID']."'>".$row['Vorname']."</option>";
-        $NACHNAME = $row['Nachname'];
+    if($params[0]=="admin" && $params[1]=="addUser"){
+        $query = "INSERT INTO `kampfsportschule`.`person` (`Vorname`,`Nachname`,`Geburtstag`,`E-Mail`) VALUES ('".$params[2]."','".$params[3]."','".$params[4]."','".$params[5]."');";
+     
+        if ($dbLink->query($query) === TRUE) {
+            return true;
+        } else {
+            echo "Error: " . $query . "<br>" . $dbLink->error;
+        return false;
+        }
+    }else if($params[1]=="delUser"){
+        $query = "DELETE FROM `kampfsportschule`.`person` WHERE  `ID`='".$params[2]."';";
+     
+        if ($dbLink->query($query) === TRUE) {
+            return true;
+        } else {
+            echo "Error: " . $query . "<br>" . $dbLink->error;
+        return false;
+        }
     }
-    
-    return $NACHNAME;
-    
 }
-/*
-if(isset($_POST['Landspeichern'])){
-    echo speichereNeuesLand($dbLink) ? "Speicherung erfolgreich" : "Speicherung fehlgeschlagen";
-}
-if(isset($_POST['ort_locSpeichern'])){
-    echo speichereOrtlage($dbLink) ? "Speicherung erfolgreich" : "Speicherung fehlgeschlagen";
-}
-*/
+ 
 
-$userID = abfrageID($dbLink);
-$userVorname = abfrageVorname($dbLink);
-$userNachname = abfrageNachname($dbLink);
+//Auflistung der Arrays aus afrageUser um die Daten nutzen zu können!
+list($id, $vorname, $nachname, $geburtstag, $email,  $erfErhaltungsdatum, $stilBeschreibung, $guertelFarbe) = abfrageUser($dbLink);
 
+$datenbankeintrag = doTraining($dbLink);
+//$editUser = doEditUser($dbLink);
+//für User
 $resultJson = [
-    "ID"=>$userID,
-    "Vorname"=>$userVorname,
-    "Nachname"=>$userNachname,
+    "ID"=>$id,
+    "Vorname"=>$vorname,
+    "Nachname"=>$nachname,
+    "Geburtsdatum"=>$geburtstag,
+    "Email"=>$email,
+    "Erfahrung in: "=>$stilBeschreibung,
+    "Guertelfarbe: "=>$guertelFarbe,
+    "Erhalten am: "=>$erfErhaltungsdatum,
+    "Training gestartet/gestopt"=>$datenbankeintrag,
     "url"=>$url,
     "params"=>$params
 ];
 
+//für Admin
+/*
+$resultJson = [
+    "Nenutzeranpassungen"=>$editUser,
+    "url"=>$url,
+    "params"=>$params
+];
+*/
 
 echo json_encode($resultJson);
-
-
-
-
-/*
-<!--
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        
-        Post-Methode aus Tabellen aus altem Projekt
-        <form action="" method="post" style="display: flex; flex-wrap: wrap">    
-        </form>
-
-        
-    </body>
-</html>
--->
-*/
