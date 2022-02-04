60dca69802a1dfe7be67a027e2ff2b771ac33b5f
BenteWTF <97910454+BenteWTF@users.noreply.github.com>
2022-02-04 09:56:30 +0100

    Update Kampfsportschule.sql

--- Kampfsportschule.sql
+++ Kampfsportschule.sql
@@ -33,7 +33,12 @@ CREATE TABLE IF NOT EXISTS `erfahrung` (
   CONSTRAINT `FK_erfahrung_stil` FOREIGN KEY (`Stil_ID`) REFERENCES `stil` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
--- Daten Export vom Benutzer nicht ausgewählt
+-- Exportiere Daten aus Tabelle kampfsportschule.erfahrung: ~2 rows (ungefähr)
+/*!40000 ALTER TABLE `erfahrung` DISABLE KEYS */;
+INSERT INTO `erfahrung` (`ID`, `Erhaltungsdatum`, `Personen_ID`, `Stil_ID`, `Guertel_ID`) VALUES
+	(3, '2011-05-06', 1, 1, 2),
+	(4, '2022-01-27', 3, 4, 3);
+/*!40000 ALTER TABLE `erfahrung` ENABLE KEYS */;
 
 -- Exportiere Struktur von Tabelle kampfsportschule.guertel
 CREATE TABLE IF NOT EXISTS `guertel` (
@@ -42,7 +47,16 @@ CREATE TABLE IF NOT EXISTS `guertel` (
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
--- Daten Export vom Benutzer nicht ausgewählt
+-- Exportiere Daten aus Tabelle kampfsportschule.guertel: ~6 rows (ungefähr)
+/*!40000 ALTER TABLE `guertel` DISABLE KEYS */;
+INSERT INTO `guertel` (`ID`, `Farbe`) VALUES
+	(1, 'Weiß'),
+	(2, 'Gelb'),
+	(3, 'Blau'),
+	(4, 'Gruen'),
+	(5, 'Braun'),
+	(6, 'Schwarz');
+/*!40000 ALTER TABLE `guertel` ENABLE KEYS */;
 
 -- Exportiere Struktur von Tabelle kampfsportschule.person
 CREATE TABLE IF NOT EXISTS `person` (
@@ -52,9 +66,16 @@ CREATE TABLE IF NOT EXISTS `person` (
   `Geburtstag` date NOT NULL,
   `E-Mail` varchar(50) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
--- Daten Export vom Benutzer nicht ausgewählt
+-- Exportiere Daten aus Tabelle kampfsportschule.person: ~2 rows (ungefähr)
+/*!40000 ALTER TABLE `person` DISABLE KEYS */;
+INSERT INTO `person` (`ID`, `Vorname`, `Nachname`, `Geburtstag`, `E-Mail`) VALUES
+	(1, 'Bente', 'Kalvelage', '1999-11-06', 'test@email.com'),
+	(2, 'Thomas', 'Lechner', '2010-01-27', 'testmail@mustermann.de'),
+	(3, 'Christian', 'Gausmann', '2012-12-01', 'mustermann@testmail.net'),
+	(6, 'Max', 'Mustermann', '1990-05-16', 'mustermann@email.com');
+/*!40000 ALTER TABLE `person` ENABLE KEYS */;
 
 -- Exportiere Struktur von Tabelle kampfsportschule.stil
 CREATE TABLE IF NOT EXISTS `stil` (
@@ -63,7 +84,15 @@ CREATE TABLE IF NOT EXISTS `stil` (
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
--- Daten Export vom Benutzer nicht ausgewählt
+-- Exportiere Daten aus Tabelle kampfsportschule.stil: ~4 rows (ungefähr)
+/*!40000 ALTER TABLE `stil` DISABLE KEYS */;
+INSERT INTO `stil` (`ID`, `Stilbezeichnung`) VALUES
+	(1, 'Karate'),
+	(2, 'Judo'),
+	(3, 'Krav Maga'),
+	(4, 'Kickboxen'),
+	(5, 'Boxen');
+/*!40000 ALTER TABLE `stil` ENABLE KEYS */;
 
 -- Exportiere Struktur von Tabelle kampfsportschule.training
 CREATE TABLE IF NOT EXISTS `training` (
@@ -77,9 +106,21 @@ CREATE TABLE IF NOT EXISTS `training` (
   KEY `FK_training_stil` (`Stil_ID`),
   CONSTRAINT `FK_training_person` FOREIGN KEY (`Personen_ID`) REFERENCES `person` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_training_stil` FOREIGN KEY (`Stil_ID`) REFERENCES `stil` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
 
--- Daten Export vom Benutzer nicht ausgewählt
+-- Exportiere Daten aus Tabelle kampfsportschule.training: ~1 rows (ungefähr)
+/*!40000 ALTER TABLE `training` DISABLE KEYS */;
+INSERT INTO `training` (`ID`, `Personen_ID`, `Zeit`, `Start/Stop`, `Stil_ID`) VALUES
+	(2, 1, '2022-02-03 12:26:01', 'Start', 1),
+	(3, 2, '1660-02-03 12:26:01', 'Start', 1),
+	(4, 2, '1665-02-03 12:26:01', 'Start', 1),
+	(5, 1, '0000-00-00 00:00:00', 'Start', 3),
+	(6, 1, '0000-00-00 00:00:00', 'Stop', 3),
+	(7, 1, '0000-00-00 00:00:00', 'Stop', 3),
+	(8, 1, '2022-02-04 08:18:53', 'Stop', 3),
+	(9, 3, '2022-02-04 08:53:59', 'Start', 2),
+	(10, 3, '2022-02-04 08:54:26', 'Stop', 2);
+/*!40000 ALTER TABLE `training` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
